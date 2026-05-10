60c3546948cea5e0f95f36d7a75e0901e080ea05
uschelz <neuheinz@web.de>
2026-05-10 22:50:37 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,26 @@
-const CACHE_NAME = "wochenplaner-cache-v4";
+const CACHE_NAME = "wochenplaner-cache-v5";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icon-blau.png"
-  "./icon-dunkelblau.png"
-  "./icon-dunkelgruen.png"
+  "./icon-blau.png",
+  "./icon-dunkelblau.png",
+  "./icon-dunkelgruen.png",
+  "./icon-olivgruen.png",
   "./icon-rot.png"
-  "./icon-olivgruen.png"
 ];
 
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(FILES_TO_CACHE);
+      return Promise.all(
+        FILES_TO_CACHE.map(function (url) {
+          return cache.add(url).catch(function (error) {
+            console.log("Konnte nicht gespeichert werden:", url, error);
+          });
+        })
+      );
     })
   );
   self.skipWaiting();
@@ -38,7 +44,17 @@ self.addEventListener("activate", function (event) {
 self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request).then(function (cachedResponse) {
-      return cachedResponse || fetch(event.request);
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      if (event.request.mode === "navigate") {
+        return caches.match("./index.html");
+      }
+
+      return fetch(event.request).catch(function () {
+        return caches.match("./index.html");
+      });
     })
   );
 });
